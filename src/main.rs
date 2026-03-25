90e6d4989ba458735e10430385ad3604bbe07d85
kunalUnit <kunalunit@users.noreply.github.com>
2026-03-25 21:06:38 +0530

    error handling in main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,15 @@
+// src/main.rs
+// Main file for the CLI File Analyser application
+// This application reads the contents of a specified file and prints it to the console.
+
+// Importing the reader module which contains the function to read files
 mod reader;
 
 fn main() {
-    let content = reader::read_file("src/main.rs").unwrap();
+    // Calling the read_file function from the reader module to read the contents of the text file and print it to the console"
+    let content = reader::read_file("src/main.rs").unwrap_or_else(|e| {
+        eprintln!("Error: {}", e);
+        std::process::exit(1);
+    });
     println!("{}", content);
 }
--- src/reader.rs
+++ src/reader.rs
@@ -1,14 +1,14 @@
 //reader.rs
 use std::{fs, io};
-use std::io::Error;
+// use std::io::Error;
 
 
 //Function to read the contents of the file and return it as a String
-pub fn read_file(file_path: &str) ->Result<String, Error> {
+pub fn read_file(file_path: &str) -> Result<String, String> {
     //read_to_string is a convenient method to read the entire contents of a file into a String
     let content = fs::read_to_string(file_path).
         map_err(|e| {
             format!("Failed to read file '{}': {}", file_path, e)
-        })
-    Ok(content)
+        });
+    content
 }
